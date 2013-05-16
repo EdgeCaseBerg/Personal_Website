999439892cad4c28660643ec59cc07c35457cd7b
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-05-16 11:22:38 -0400

    Added social bar on right and ajax loading images. Working on Feeds parsing

--- index.php
+++ index.php
@@ -38,23 +38,70 @@
 		</div>
 
 		<div class="main clearBoth left">
-			<div class="left" >
+			<div class="left" id="leftContent">
 				<h2>About me...</h2>
 				<p>
-					This is where I would put a trophy. IF I HAD ONE Lorem ipsum jasdfjasdkfjaskdljf askjf askldj falksd jfalksd fjalkd jfla;sdjf ;lakjd flaksdj fakd fjaklsd fjakld fjakld fjakld fjalskd fjaklds fjaksd fjalksdjf alksdfjadklf jasdkf jasdkl fjasdlkfjad;lfkja;dlkfjadklfjadklfja
+
 				</p>
 			</div>
-			<div class="right"> 
+			<div class="right" id="rightContent"> 
 				<h2>Recent Activity</h2>
-				<ul>
-					<li class="icon"><a href="http://ethaneldridgecs.blogspot.com/"><img src="images/blogger.png" /></a></li>
-					<li class="icon"><a href="https://github.com/EJEHardenberg"><img src="images/github.png"></a></li>
-					<li class="icon"><a href="http://www.linkedin.com/profile/view?id=151414806"><img src="images/LinkedIn_logo.png" ></a></li>
-					<li class="icon"><a href="https://twitter.com/EthanJEldridge"><img src="images/twitter-bird-light-bgs.png" /></a></li>	
-				</ul>
+				<dl class="social">
+					<dt class="social icon"><a href="http://ethaneldridgecs.blogspot.com/"><img src="images/blogger.png" /></a></dt>
+					<dd id="#blogRSS"><img class="loader" src="images/ajax-loader.gif" /></dd>
+					<dt class="social icon"><a href="https://github.com/EJEHardenberg"><img src="images/github.png"></a></dt>
+					<dd id="#githubRSS"><img class="loader" src="images/ajax-loader.gif" /></dd>
+					<dt class="social icon"><a href="http://www.linkedin.com/profile/view?id=151414806"><img src="images/LinkedIn_logo.png" ></a></dt>
+					<dd id="#linkedInRSS"><img class="loader" src="images/ajax-loader.gif" /></dd>
+					<dt class="social icon"><a href="https://twitter.com/EthanJEldridge"><img src="images/twitter-bird-light-bgs.png" /></a></dt>	
+					<dd id="#twitterRSS"><img class="loader" src="images/ajax-loader.gif" /></dd>
+				</dl>
 			</div>
 		</div>
 
+	<script type="text/javascript">
+		//Blogger RSS
+		var blog = 'http://ethaneldridgecs.blogspot.com/feeds/posts/default';
+		//Linked in RSS
+		var linked = 'http://www.linkedin.com/rss/nus?key=t4rHAZ4ZAdKlwX4OUS8ca-4c3fnoKuV8ZHMG5Ma2-BoVAC1xvFF0GYgwuQa8s4LrIcr';
+		//Github RSS
+		var github = 'https://github.com/EJEHardenberg.atom';
+		//Twitter RSS
+		var twitter = 'http://www.twitter-rss.com/user_timeline.php?screen_name=ethanjeldridge';
+		
+
+		//Helper function to fetch URL contents
+		function httpGet(theUrl,feedName){
+    		var xmlHttp = null;
+    		xmlHttp = new XMLHttpRequest();
+    		xmlHttp.onreadystatechange = function(){processGET(xmlHttp,feedName)};
+    		xmlHttp.open( "GET", 'scripts/proxy.php?url='+theUrl, true );
+    		xmlHttp.send( null );
+		}
+		function processGET(xmlHttp,feedName){
+			//Process page only if ready
+			if ( xmlHttp.readyState == 4 && xmlHttp.status == 200 ){
+				var page = xmlHttp.responseText;
+				switch(feedName){
+					case 'blog':
+						blogRead(page);
+						break;
+					case 'linked':
+						break;
+					case 'github':
+						break;
+					case 'twitter':
+						break;
+				}
+			}
+		}
+		function blogRead(page){
+			
+		}
+
+		httpGet('http://ethaneldridgecs.blogspot.com/feeds/posts/default','blog');
+
+	</script>
 	</body>
 
 </html>
