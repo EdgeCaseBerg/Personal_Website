0ecb3f40d4d9d47ae7d472c92238d1f2bada9079
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-05-16 00:10:24 -0400

    Added beginning index pages, commented out analytics, basic css

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE  html>
+
+	<head>
+		<title> Ethan Joachim Eldridge's Webspace</title>
+		<meta name="description" content="Ethan Eldridges personal website, projects, resume, and contact information for Ethan Eldridge.">
+		<meta name="keywords" content="Ethan Eldridge,Ethan,Eldridge,UVM,ejeldrid,Computer Science,Cluster,CS,Computer">
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<script type="text/javascript">
+			/*
+  			var _gaq = _gaq || [];
+  			_gaq.push(['_setAccount', 'UA-32336195-1']);
+  			_gaq.push(['_trackPageview']);
+
+  			(function() {
+    			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  			})();
+  			*/
+		</script>
+	</head>
+
+	<body>
+
+		<div class="header left clearBoth">
+			<h1>Welcome to Ethan Eldridge's Personal Website</h1>
+		</div>
+
+
+	</body>
+
+</html>
